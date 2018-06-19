19f67156ae2d602113766c68cdf2819a89e36b96
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-19 14:16:14 -0400

    Added logic for 'Add Question' button

--- app/src/main/java/com/example/ethanrowe/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/ethanrowe/triviaapp/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.ethanrowe.triviaapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @OnClick(R.id.save_question_button)
+    protected void addQuestionClicked() {
+
+        questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+
+    }
+
 }
