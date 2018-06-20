2ba6f6ccb06c35a4716cff02bf1d6eff6d789131
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-20 15:49:35 -0400

    Added if-else logic and the toast if the 'if' statement is violated

--- app/src/main/java/com/example/ethanrowe/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/ethanrowe/triviaapp/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.ethanrowe.triviaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements QuestionCreatorFragment.Callback{
 
     private QuestionCreatorFragment questionCreatorFragment;
+    private List<Question> questionsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        questionsList = new ArrayList<>();
     }
 
     @OnClick(R.id.save_question_button)
@@ -22,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
 
+
+
     }
 
+    @Override
+    public void questionSaved(Question question) {
+
+        questionsList.add(question);
+        Toast.makeText(this, "Question Saved!", Toast.LENGTH_SHORT).show();
+
+        getSupportFragmentManager().beginTransaction().remove(questionCreatorFragment).commit();
+    }
 }
--- app/src/main/java/com/example/ethanrowe/triviaapp/QuestionCreatorFragment.java
+++ app/src/main/java/com/example/ethanrowe/triviaapp/QuestionCreatorFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,15 +19,15 @@ public class QuestionCreatorFragment extends Fragment {
     @BindView(R.id.question_edittext)
     protected EditText question;
     @BindView(R.id.correct_answer_edittext)
-    protected EditText correctAnswer;
+    protected EditText correctAnswerInput;
     @BindView(R.id.first_wrong_answer_edittext)
-    protected EditText firstWrongAnswer;
+    protected EditText firstWrongAnswerInput;
     @BindView(R.id.second_wrong_answer_edittext)
-    protected EditText secondWrongAnswer;
+    protected EditText secondWrongAnswerInput;
     @BindView(R.id.third_wrong_answer)
-    protected EditText thirdWrongAnswer;
+    protected EditText thirdWrongAnswerInput;
 
-
+    private Callback callback;
 
 
     @Nullable
@@ -50,8 +51,35 @@ public class QuestionCreatorFragment extends Fragment {
     @OnClick(R.id.save_question_button)
     protected void addQuestion() {
 
-        String questionTitle = question.getText().toString();
-        
+        if (question.getText().toString().isEmpty() || correctAnswerInput.getText().toString().isEmpty() || firstWrongAnswerInput.getText().toString().isEmpty() || secondWrongAnswerInput.getText().toString().isEmpty() || thirdWrongAnswerInput.getText().toString().isEmpty()) {
+
+            Toast.makeText(getActivity(), "All fields are required!", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            //Gets user input from the EditTexts
+            String questionTitle = question.getText().toString();
+            String correctAnswer = correctAnswerInput.getText().toString();
+            String firstWrongAnswer = firstWrongAnswerInput.getText().toString();
+            String secondWrongAnswer = secondWrongAnswerInput.getText().toString();
+            String thirdWrongAnswer = thirdWrongAnswerInput.getText().toString();
+
+            //Takes Variables created from user input and saves them in the Question object
+            Question question = new Question(questionTitle, correctAnswer, firstWrongAnswer, secondWrongAnswer, thirdWrongAnswer);
+
+            //Sends question object we just created to the callback method to be saved.
+            callback.questionSaved(question);
+
+        }
+    }
+
+    public void attachParent(Callback callback) {
+        this.callback = callback;
 
     }
+
+    public interface Callback {
+        void questionSaved(Question question);
+    }
 }
+
